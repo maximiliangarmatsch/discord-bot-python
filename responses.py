c83d4156243ab77ea6926c3dff280fa8969c3390
USTADCOM <amirsohailali123@gmail.com>
2024-06-01 13:33:16 +0500

    Update response.py

--- responses.py
+++ responses.py
@@ -13,7 +13,19 @@ client = OpenAI(api_key = OPENAI_API_KEY)
 
 def get_response(user_input: str, model = "gpt-3.5-turbo") -> str:
     """
-    doc string
+    Openai response method take user query and return its response.
+
+    Parameters
+    ----------
+    user_input: str
+        user query or input.
+    model: str
+        openai model name.
+
+    Return
+    ------
+    response: str
+        openai response for each user query.
     """
     response = client.chat.completions.create(
             model="gpt-3.5-turbo",
